05c7db160ae691104f1c4c3ff06228b250ece111
Lu Lu <lulu@Lu.local>
2024-09-27 14:47:33 +0200

    updated index.js mouting mealrouter

--- api/src/index.js
+++ api/src/index.js
@@ -9,36 +9,27 @@ import { firstMeal } from "./routers/first-meal.js";
 import { futureMeal } from "./routers/future-meals.js";
 import { lastMeal } from "./routers/last-meal.js";
 import { pastMeal } from "./routers/past-meals.js";
-
+import mealsRouter from "./routers/meals.js"; // Import the new meals router
 const app = express();
 app.use(cors());
 app.use(bodyParser.json());
 
 const apiRouter = express.Router();
 
-// You can delete this route once you add your own routes
 apiRouter.get("/", async (req, res) => {
-  
-  res.json({ messages:'welcome to meal sharing API'
-   });
+  res.json({ messages: "welcome to meal sharing API" });
 });
 
 // This nested router example can also be replaced with your own sub-router
-apiRouter.use("/future-meals", futureMeal );
-apiRouter.use("/past-meals", pastMeal );
-apiRouter.use("/all-meals", allMeals );
-apiRouter.use("/first-meal", firstMeal );
-apiRouter.use("/last-meal", lastMeal );
-
+apiRouter.use("/future-meals", futureMeal);
+apiRouter.use("/past-meals", pastMeal);
+apiRouter.use("/all-meals", allMeals);
+apiRouter.use("/first-meal", firstMeal);
+apiRouter.use("/last-meal", lastMeal);
+apiRouter.use("/meals", mealsRouter);
 
 app.use("/api", apiRouter);
 
 app.listen(process.env.PORT, () => {
   console.log(`API listening on port ${process.env.PORT}`);
 });
-
-import mealsRouter from './routers/meals.js';  // Import the new meals router
-
-// Add this line to use the meals router
-apiRouter.use('/meals', mealsRouter);
-
