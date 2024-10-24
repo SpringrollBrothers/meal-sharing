2b4dd4175f6f70455cfa9f414dd454ec3ba83a76
SpringrollBrothers <158217780+SpringrollBrothers@users.noreply.github.com>
2024-10-24 10:53:30 +0200

    Update api/src/routers/first-meal.js
    Co-authored-by: Gilson Urbano <guf@hotmail.com.br>

--- api/src/routers/first-meal.js
+++ api/src/routers/first-meal.js
@@ -4,7 +4,7 @@ import knex from "../database_client.js";
 
 const firstMeal = express.Router();
 firstMeal.get('/', async(req , res)=> {
-    const first_meal = await knex('meal').orderBy('id').limit(1)
+    const first_meal = await knex('meal').orderBy('id').first()
     res.json(first_meal)
 });
 export {firstMeal}
