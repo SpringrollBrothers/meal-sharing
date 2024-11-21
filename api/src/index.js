e574318eddbfd7473b1a6bdada1b3a42edccdc33
Lu Lu <lulu@Lu.local>
2024-11-21 10:21:08 +0100

    added review and updated index.js

--- api/src/index.js
+++ api/src/index.js
@@ -3,13 +3,13 @@ import express from "express";
 import cors from "cors";
 import bodyParser from "body-parser";
 import knex from "./database_client.js";
-import nestedRouter from "./routers/nested.js";
 import { allMeals } from "./routers/all-meals.js";
 import { firstMeal } from "./routers/first-meal.js";
 import { futureMeal } from "./routers/future-meals.js";
 import { lastMeal } from "./routers/last-meal.js";
 import { pastMeal } from "./routers/past-meals.js";
 import mealsRouter from "./routers/meals.js"; // Import the new meals router
+import reviewRouter from "./routers/review.js";
 const app = express();
 app.use(cors());
 app.use(bodyParser.json());
@@ -27,10 +27,10 @@ apiRouter.use("/all-meals", allMeals);
 apiRouter.use("/first-meal", firstMeal);
 apiRouter.use("/last-meal", lastMeal);
 apiRouter.use("/meals", mealsRouter);
+apiRouter.use("/review",reviewRouter)
 
 app.use("/api", apiRouter);
 
 app.listen(process.env.PORT, () => {
   console.log(`API listening on port ${process.env.PORT}`);
 });
-
--- api/src/routers/review.js
+++ api/src/routers/review.js
@@ -1,25 +1,67 @@
 import express from "express";
 import knex from "../database_client.js";
-
-// This router can be deleted once you add your own router
 const reviewRouter = express.Router();
 
-reviewRouter.get("/", async (req, res) => {
-  const SHOW_TABLES_QUERY = "SELECT * FROM review;";
+//return all reviews
+reviewRouter.get("/", async (req, res, next) => {
+  try {
+    const reviews = await knex("review");
+    res.json(reviews);
+  } catch (error) {
+    next(error);
+  }
+});
+//return a review by Id
+reviewRouter.get("/:id", async (req, res, next) => {
+  try {
+    const id = req.params.id;
+    const getReviewById = await knex("review").where({ id }).first();
+    if (!getReviewById) {
+      return res.status(404).json({ error: "review not found." });
+    }
 
-  const tables = await knex.raw(SHOW_TABLES_QUERY);
-
-  res.json({ tables });
+    res.json(getReviewById);
+  } catch (error) {
+    next(error);
+  }
 });
 
-reviewRouter.get("/:id", async (req, res) => {
-  const SHOW_TABLES_QUERY =
-    `SELECT * FROM review where id =${req.params.id}`
-
-  console.log(SHOW_TABLES_QUERY);
-
-  const tables = await knex.raw(SHOW_TABLES_QUERY);
-  const tablerows = tables[0];
-  res.json({ tablerows });
+//to add a new review to database.
+reviewRouter.post("/", async (req, res, next) => {
+  try {
+    const nreMeal = req.body;
+    const [mealId] = await knex("meal").insert(newMeal);
+    res.status(201).json({ message: "Meal created", mealId });
+  } catch (error) {
+    next(error);
+  }
 });
+
+//updates the review by Id
+reviewRouter.put("/:id", async (req, res, next) => {
+  try {
+    const id = req.params.id;
+    const updateReview = req.body;
+    await knex("review").where({ id }).update(updateReview);
+    res.json({ message: "Review updated" });
+  } catch (error) {
+    next(error);
+  }
+});
+
+//Delete the review by Id.
+reviewRouter.delete("/:id", async (req, res, next) => {
+  try {
+    const id = req.params.id;
+    const deleteReview = await knex("review").where({ id }).del();
+    if (!deleteReview) {
+      res.status(404).json({ errror: "Not found" });
+    } else {
+      res.status(200).json({ message: "Review deleted" });
+    }
+  } catch (error) {
+    next(error);
+  }
+});
+
 export default reviewRouter;
