ff994140e7c4e9004f300e81f9adea08cc021ac7
Lu Lu <lulu@Lu.local>
2024-12-02 15:32:46 +0100

    added button leading to meals details,added dynamic ID for router

--- frontend/app/components/Mealcard.jsx
+++ frontend/app/components/Mealcard.jsx
@@ -1,13 +1,24 @@
 import React from "react";
-import styles from "../mealslist/page.module.css"
+import styles from "../mealslist/page.module.css";
+import { useRouter } from "next/navigation";
 
 function Meal({ meal }) {
+  const router = useRouter();
+  function handleInfo() {
+    router.push(`/mealslist/${meal.id}`);
+  }
   return (
     <>
       <div className={styles.mealCard}>
+        <div className={styles.img}>
+          <img src={meal.image_url} alt={meal.title} />
+        </div>
         <h3 className={styles.mealTitle}>{meal.title}</h3>
         <p className={styles.mealDesc}>{meal.DESCRIPTION}</p>
         <p className={styles.mealPrice}>{meal.price}</p>
+        <button onClick={handleInfo} className={styles.mealBtn}>
+          More Info
+        </button>
       </div>
     </>
   );
---
+++ frontend/app/mealslist/[id]/page.jsx
@@ -0,0 +1,8 @@
+import React from "react"
+function MealId(){
+return(
+    <h1>MealId</h1>
+)
+}
+
+export default MealId
